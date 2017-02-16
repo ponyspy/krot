3a92619b91fa80e41bdc5188e58750e5da1969c0
ponyspy <desade4me@gmail.com>
2017-02-16 21:38:42 +0300

    remove

--- apps/krot/routes/admin/articles/remove.js
+++ apps/krot/routes/admin/articles/remove.js
@@ -4,21 +4,26 @@ module.exports = function(Model) {
 	var module = {};
 
 	var Article = Model.Article;
+	var Issue = Model.Issue;
 
 
 	module.index = function(req, res, next) {
 		var id = req.body.id;
 
-		Article.findByIdAndRemove(id).exec(function(err) {
+		Issue.update({'columns.$.articles': id}, { $pull: { 'columns.articles': id } }, { multi: true }).exec(function(err) {
 			if (err) return next(err);
 
-			rimraf(__glob_root + '/public/cdn/' + __app_name + '/images/articles/' + id, { glob: false }, function(err) {
+			Article.findByIdAndRemove(id).exec(function(err) {
 				if (err) return next(err);
 
-				rimraf(__glob_root + '/public/cdn/' + __app_name + '/files/articles/' + id, { glob: false }, function(err) {
+				rimraf(__glob_root + '/public/cdn/' + __app_name + '/images/articles/' + id, { glob: false }, function(err) {
 					if (err) return next(err);
 
-					res.send('ok');
+					rimraf(__glob_root + '/public/cdn/' + __app_name + '/files/articles/' + id, { glob: false }, function(err) {
+						if (err) return next(err);
+
+						res.send('ok');
+					});
 				});
 			});
 		});
