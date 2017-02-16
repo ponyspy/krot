205a7409cc31e09a38ced2e550989cbb165249df
ponyspy <desade4me@gmail.com>
2017-02-16 21:58:53 +0300

    async

--- apps/krot/routes/admin/issues/remove.js
+++ apps/krot/routes/admin/issues/remove.js
@@ -1,4 +1,5 @@
 var rimraf = require('rimraf');
+var async = require('async');
 
 module.exports = function(Model) {
 	var module = {};
@@ -9,16 +10,18 @@ module.exports = function(Model) {
 	module.index = function(req, res, next) {
 		var id = req.body.id;
 
-		Issue.findByIdAndRemove(id).exec(function(err) {
+		async.parallel([
+			function(callback) {
+				Issue.findByIdAndRemove(id).exec(callback);
+			},
+			function(callback) {
+				rimraf(__glob_root + '/public/cdn/' + __app_name + '/images/issues/' + id, { glob: false }, callback);
+			}
+		], function(err, results) {
 			if (err) return next(err);
 
-			rimraf(__glob_root + '/public/cdn/' + __app_name + '/images/issues/' + id, { glob: false }, function(err) {
-				if (err) return next(err);
-
-				res.send('ok');
-			});
+			res.send('ok');
 		});
-
 	};
 
 
