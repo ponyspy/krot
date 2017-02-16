eada61fdc6db950c63d85e5e29f20bb948c3fcec
ponyspy <desade4me@gmail.com>
2017-02-16 21:29:20 +0300

    category remove

--- apps/krot/routes/admin/categorys/remove.js
+++ apps/krot/routes/admin/categorys/remove.js
@@ -4,16 +4,16 @@ module.exports = function(Model) {
 	var module = {};
 
 	var Category = Model.Category;
-	// var Event = Model.Event;
+	var Article = Model.Article;
 
 
 	module.index = function(req, res, next) {
 		var id = req.body.id;
 
 		async.parallel([
-			// function(callback) {
-			// 	Event.update({'categorys': id}, { $pull: { 'categorys': id } }, { 'multi': true }).exec(callback);
-			// },
+			function(callback) {
+				Article.update({'categorys': id}, { $pull: { 'categorys': id } }, { 'multi': true }).exec(callback);
+			},
 			function(callback) {
 				Category.findByIdAndRemove(id).exec(callback);
 			}
