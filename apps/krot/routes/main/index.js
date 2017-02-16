6dfbf4bfea978670003dd24e7a24d0c3c7e897d0
ponyspy <desade4me@gmail.com>
2017-02-16 20:55:08 +0300

    fix

--- apps/krot/routes/main/index.js
+++ apps/krot/routes/main/index.js
@@ -5,7 +5,7 @@ module.exports = function(Model) {
 
 	module.index = function(req, res, next) {
 		Issue.where('status').ne('hidden').sort('-date').skip(0).limit(1).exec(function(err, issues) {
-			if (!issue || !issue.length) return next(err);
+			if (!issues || !issues.length) return next(err);
 
 			res.redirect('/issues/' + issues[0].numb || issues[0]._short_id);
 		});
