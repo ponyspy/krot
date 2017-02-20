6e0f1a879598e44e10390554437cb5ac784bfa12
ponyspy <desade4me@gmail.com>
2017-02-20 15:44:31 +0300

    main

--- apps/krot/routes/main/issues/issue.js
+++ apps/krot/routes/main/issues/issue.js
@@ -8,7 +8,14 @@ module.exports = function(Model) {
 	module.index = function(req, res, next) {
 		var id = req.params.issue_id;
 
-		Issue.find({ $or: [ { '_short_id': id }, { 'numb': id } ] }).exec(function(err, issues) {
+		Issue.find({ $or: [ { '_short_id': id }, { 'numb': id } ] }).populate({
+			path: 'columns.articles',
+			select: 'base hover categorys _short_id',
+			populate: {
+				path: 'categorys',
+				select: 'title _short_id'
+			}
+		}).exec(function(err, issues) {
 			if (!issues || !issues.length) return next(err);
 
 			res.render('main/issues/issue.jade', { issue: issues[0] });
