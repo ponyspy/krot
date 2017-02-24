e4e37a50f0fda26b1b3c4e458306445cd3f68e1e
ponyspy <desade4me@gmail.com>
2017-02-24 18:00:52 +0300

    init

--- apps/krot/src/js/issue.js
+++ apps/krot/src/js/issue.js
@@ -1,3 +1,21 @@
 $(function() {
-
+	var swiperH = new Swiper('.swiper-container-h', {
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev',
+		slidesPerView: 'auto',
+		initialSlide: 1,
+		direction: 'horizontal',
+		spaceBetween: 20,
+		autoHeight: true,
+		// speed: 800,
+		// autoplay: 4000,
+		// autoplayDisableOnInteraction: false,
+		// effect: 'fade',
+		// autoHeight: true,
+		// initialSlide: 1,
+		centeredSlides: true,
+		keyboardControl: false,
+		loop: false,
+		// runCallbacksOnInit: false,
+	});
 });
