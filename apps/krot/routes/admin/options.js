6eee792dfbab4f8d699d500adaa2969b2b176f64
ponyspy <desade4me@gmail.com>
2017-03-11 19:42:38 +0300

    svg article

--- apps/krot/routes/admin/options.js
+++ apps/krot/routes/admin/options.js
@@ -1,18 +1,39 @@
+var fs = require('fs');
 var gm = require('gm').subClass({ imageMagick: true });
+var mime = require('mime');
+var svgo = require('svgo');
 var rimraf = require('rimraf');
 
 module.exports.preview = function(req, res) {
 	var file = req.file;
+	var file_name = Date.now();
 
-	gm(file.path).identify({ bufferStream: true }, function(err, meta) {
-		var new_path = '/preview/' + Date.now() + '.' + (meta['Channel depth'].Alpha ? 'png' : 'jpg');
+	if (mime.extension(file.mimetype) == 'svg') {
+		var SVGO = new svgo({
+			plugins: [{
+				convertShapeToPath: false
+			}]
+		});
+		var new_path = '/preview/' + file_name + '.svg';
 
-		this.resize(meta.size.width > 600 ? 600 : false, false);
-		this.quality(meta.size.width >= 600 ? 82 : 100);
-		this.write(__glob_root + '/public' + new_path, function (err) {
-			rimraf(file.path, { glob: false }, function() {
-				res.send(new_path);
+		fs.readFile(file.path, function(err, data) {
+			SVGO.optimize(data, function(result) {
+				fs.writeFile(__glob_root + '/public' + new_path, result.data, function(err) {
+					res.send(new_path);
+				});
 			});
 		});
-	});
+	} else if (/jpeg|png/.test(mime.extension(file.mimetype))) {
+		gm(file.path).identify({ bufferStream: true }, function(err, meta) {
+			var new_path = '/preview/' + file_name + '.' + (meta['Channel depth'].Alpha ? 'png' : 'jpg');
+
+			this.resize(meta.size.width > 600 ? 600 : false, false);
+			this.quality(meta.size.width >= 600 ? 82 : 100);
+			this.write(__glob_root + '/public' + new_path, function (err) {
+				rimraf(file.path, { glob: false }, function() {
+					res.send(new_path);
+				});
+			});
+		});
+	}
 };
