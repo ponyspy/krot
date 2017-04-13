b447e7c3cae70a9a1cf054f24932ad0785137b60
ponyspy <desade4me@gmail.com>
2017-04-13 21:12:16 +0300

    state scroll

--- apps/krot/src/js/article.js
+++ apps/krot/src/js/article.js
@@ -1,3 +1,15 @@
 $(function() {
+	var $document = $(document);
+
+	$(window)
+		.on('load', function(e) {
+			$document.scrollTop(history.state.scroll || 0);
+		})
+		.on('scroll', function(e) {
+			clearTimeout($.data(this, 'scrollTimer'));
+			$.data(this, 'scrollTimer', setTimeout(function() {
+				history.pushState({ scroll: $document.scrollTop() }, 'scroll');
+			}, 250));
+		});
 
 });
