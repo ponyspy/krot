436f01726b6abaae9040587c5c3d812b1135868f
ponyspy <desade4me@gmail.com>
2017-04-28 09:19:14 +0300

    gif

--- apps/krot/routes/admin/_params/upload.js
+++ apps/krot/routes/admin/_params/upload.js
@@ -43,9 +43,11 @@ module.exports.image = function(obj, base_path, field_name, file_size, file, del
 						});
 					});
 				});
-			} else if (/jpeg|png/.test(mime.extension(file.mimetype))) {
+			} else if (/jpeg|png|gif/.test(mime.extension(file.mimetype))) {
 				gm(file.path).identify({ bufferStream: true }, function(err, meta) {
-					var file_name = field_name + '.' + ((meta['Channel depth'].Alpha || meta['Channel statistics'].Alpha || meta.Alpha) ? 'png' : 'jpg');
+					var file_name = mime.extension(file.mimetype) == 'gif'
+						? field_name + '.gif'
+						: field_name + '.' + ((meta['Channel depth'].Alpha || meta['Channel statistics'].Alpha || meta.Alpha) ? 'png' : 'jpg');
 
 					this.resize(meta.size.width > file_size ? file_size : false, false);
 					this.quality(meta.size.width >= file_size ? 82 : 100);
--- apps/krot/routes/admin/options.js
+++ apps/krot/routes/admin/options.js
@@ -25,9 +25,11 @@ module.exports.preview = function(req, res) {
 				});
 			});
 		});
-	} else if (/jpeg|png/.test(mime.extension(file.mimetype))) {
+	} else if (/jpeg|png|gif/.test(mime.extension(file.mimetype))) {
 		gm(file.path).identify({ bufferStream: true }, function(err, meta) {
-			var new_path = '/preview/' + file_name + '.' + ((meta['Channel depth'].Alpha || meta['Channel statistics'].Alpha || meta.Alpha) ? 'png' : 'jpg');
+			var new_path = mime.extension(file.mimetype) == 'gif'
+				? '/preview/' + file_name + '.gif'
+				: '/preview/' + file_name + '.' + ((meta['Channel depth'].Alpha || meta['Channel statistics'].Alpha || meta.Alpha) ? 'png' : 'jpg');
 
 			this.resize(meta.size.width > 600 ? 600 : false, false);
 			this.quality(meta.size.width >= 600 ? 82 : 100);
