0740e4577a1931163d4d73efe39320e312bad561
ponyspy <desade4me@gmail.com>
2019-08-30 02:49:51 +0300

    fix

--- apps/krot/routes/admin/mirrors/options.js
+++ apps/krot/routes/admin/mirrors/options.js
@@ -9,16 +9,19 @@ module.exports = function(Model) {
 
 
 	module.mirrors_apply = function(req, res) {
+		var conf_path = process.env.NODE_ENV == 'production' ? '/etc/nginx/vhosts/mirrors.conf' : __glob_root + '/test.conf';
+		var run_cmd = process.env.NODE_ENV == 'production' ? 'systemctl restart nginx' : 'ls';
+
 		Mirror.find().where('status').ne('hidden').sort('-date').exec(function(err, mirrors) {
 
 			if (err || !mirrors || mirrors.length == 0) {
-				rimraf(__glob_root + '/out.conf', function() { // /etc/nginx/vhosts/mirrors.conf
-					exec('ls', function(err, stdout, stderr) { // systemctl restart nginx
+				rimraf(conf_path, function() {
+					exec(run_cmd, function(err, stdout, stderr) { // systemctl restart nginx
 						res.send('fail');
 					});
 				});
 			} else {
-				fs.readFile(__glob_root + '/mirrors_tmpl.conf', 'utf8', function(err, data) {
+				fs.readFile(__glob_root + '/m_tmpl.conf', 'utf8', function(err, data) {
 					if (err) throw err;
 
 					var domains = mirrors.map(function(m) { return m.name; });
@@ -27,8 +30,8 @@ module.exports = function(Model) {
 					data = data.replace(/@mirrors@/g, domains.join(' '));
 					data = data.replace(/@www_mirrors@/g, www.join(' '));
 
-					fs.writeFile(__glob_root + '/out.conf', data, function(err) { // /etc/nginx/vhosts/mirrors.conf
-						exec('ls', function(err, stdout, stderr) { // systemctl restart nginx
+					fs.writeFile(conf_path, data, function(err) { // /etc/nginx/vhosts/mirrors.conf
+						exec(run_cmd, function(err, stdout, stderr) { // systemctl restart nginx
 							res.send('ok');
 						});
 					});
