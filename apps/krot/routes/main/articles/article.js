af262f5640f04396f0d9b315d18e6d7b88a3650a
ponyspy <desade4me@gmail.com>
2020-03-02 22:22:33 +0300

    header

--- apps/krot/routes/main/articles/article.js
+++ apps/krot/routes/main/articles/article.js
@@ -26,7 +26,7 @@ module.exports = function(Model) {
 				Question.aggregate().match({'status': {'$ne': 'hidden'}, '_short_id': {'$nin': hole_right}}).sample(1).exec(function(err, question) {
 					if (err || !question.length) return next(err);
 
-					res.set('X-Update', 'yes');
+					res.set({'X-Update': 'yes', 'Cache-Control': 'no-store, must-revalidate', 'Pragma': 'no-cache', 'Expires': '0'});
 					return res.status(404).render('main/articles/hole.pug', {question: question[0]});
 				});
 			} else {
@@ -65,7 +65,7 @@ module.exports = function(Model) {
 			}
 
 			question.save(function(err) {
-				res.set('X-Update', 'yes');
+				res.set({'X-Update': 'yes', 'Cache-Control': 'no-store, must-revalidate', 'Pragma': 'no-cache', 'Expires': '0'});
 				res.send('ok');
 			});
 		});
